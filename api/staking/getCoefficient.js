68dc1e3137c49ea779c8e66a3697d594df509baa
martin-0x11 <martin@0x11.email>
2021-10-15 15:34:54 +0800

    update table

--- api/staking/getCoefficient.js
+++ api/staking/getCoefficient.js
@@ -1,8 +1,8 @@
 const sequelizer = require("../../config/mysql2");
 const result = require("../../utils/Result");
-require("../../model/staking_stake");
+require("../../model/staking_coefficient");
 
 module.exports = async (req, res) => {
-    data = await sequelizer.models.StakingStake.findAll({});
+    data = await sequelizer.models.StakingCoefficient.findAll({});
     res.status(200).json(result.success(data));
 }
