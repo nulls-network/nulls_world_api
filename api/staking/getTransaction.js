505e4a1311df9bb66b4e792d3717240f38be756a
martin-0x11 <martin@0x11.email>
2021-10-15 13:48:00 +0800

    event Optional

--- api/staking/getTransaction.js
+++ api/staking/getTransaction.js
@@ -3,10 +3,15 @@ const result = require("../../utils/Result");
 require("../../model/staking_transaction");
 
 module.exports = async (req, res) => {
-    let address = req.query.address;
     let event = req.query.event;
+    let where = {
+        address: req.query.address
+    }
+    if (event) {
+        where.event = event
+    }
     data = await sequelizer.models.StakingTransaction.findAll({
-        where: { address: address, event: event }
+        where: where
     });
     res.status(200).json(result.success(data));
 }
