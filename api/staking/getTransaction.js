a707b527503d00a91288a7d7fe3f3ec754863445
martin-0x11 <martin@0x11.email>
2021-10-16 17:01:27 +0800

    events fix

--- api/staking/getTransaction.js
+++ api/staking/getTransaction.js
@@ -5,14 +5,20 @@ const { Op } = require("sequelize");
 
 module.exports = async (req, res) => {
     let events = req.query["events[]"];
+    if (!events) {
+        events = req.query["events"];
+    }
     console.log(req.query)
     console.log("==============")
-    console.log(events)
+    console.log(typeof events);
+    if (typeof events === "string") {
+        events = events.split(',');
+    }
     let where = {
         address: req.query.address
     }
     if (events) {
-        where.event =  {[Op.in]: events }
+        where.event = { [Op.in]: events }
     }
     data = await sequelizer.models.StakingTransaction.findAll({
         where: where
