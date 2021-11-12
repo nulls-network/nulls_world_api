f59d557e4dc4ca778d3708d997ef062b001adc16
slightly-null <91324295+slightly-null@users.noreply.github.com>
2021-11-12 13:51:41 +0800

    ring comiit  add return  requestkey. txhash

--- api/index/findRankCommit.js
+++ api/index/findRankCommit.js
@@ -14,7 +14,7 @@ module.exports = async (req, res) => {
             offer = Number((current-1)*pageSize);
             limit = Number(pageSize)
     }
-    const sql = "select  icl.item_id,icl.extend,icl.token,icl.token_name,icl.token_precision,r.owner_address as ring_address, r.pet_id as ring_pet_id ,pet.pet_id as challenger_pet_id,pet.owner_address challenger_address,icl.create_time from  item_commit_log  icl left join  ring r on icl.item_id = r.item_id left join pet on  icl.challengerPetId = pet.pet_id where  icl.module = 2  and icl.user = ? and   icl.status =  ? ";
+    const sql = "select  icl.requestKey,icl.tx_hash, icl.item_id,icl.extend,icl.token,icl.token_name,icl.token_precision,r.owner_address as ring_address, r.pet_id as ring_pet_id ,pet.pet_id as challenger_pet_id,pet.owner_address challenger_address,icl.create_time from  item_commit_log  icl left join  ring r on icl.item_id = r.item_id left join pet on  icl.challengerPetId = pet.pet_id where  icl.module = 2  and icl.user = ? and   icl.status =  ? ";
     const countsql = "select count(icl.item_id) from  item_commit_log  icl left join  ring r on icl.item_id = r.item_id left join pet on  icl.challengerPetId = pet.pet_id where  icl.module = 2  and icl.user = ? and   icl.status =  ? ";
 
     const  ordersql = " order by deadline asc"
