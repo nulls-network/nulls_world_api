6f412a81a3a924105d7f6e23d8d5b7d11607c7e5
slightly-null <slightly@0x11.email>
2021-11-29 17:46:46 +0800

    error  code

--- api/email/getToken.js
+++ api/email/getToken.js
@@ -9,12 +9,12 @@ module.exports = async (req, res) => {
     const {email, address , vercode } = req.query;
     var pattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
     if (!pattern.test(email)){
-        res.status(200).json(result.success("email  Invalid parameter"));
+        res.status(200).json(result.error("email  Invalid parameter"));
         return
     }
     redis.hgetall(email,async function(err,data){
         if(err){
-            res.status(200).json(result.success(`redis server  error  ${err}`));
+            res.status(200).json(result.error(`redis server  error  ${err}`));
             return
         }
         if(data){
@@ -22,18 +22,17 @@ module.exports = async (req, res) => {
             const nowtime = await dateUtils.getBlockTimeMillisecond();
             
             if(data.get_time !=undefined && Number(data.get_time) > nowtime - 24*60*60*1000){
-                res.status(200).json(result.success("It was picked up within 24 hours, come back later"));
+                res.status(200).json(result.error("It was picked up within 24 hours, come back later"));
                 return
             }
             if(data.random !=vercode){
-                res.status(200).json(result.success("vercode  Invalid parameter"));
+                res.status(200).json(result.error("vercode  Invalid parameter"));
                 return
             }
             if(data.status === '2'){
-                res.status(200).json(result.success("The verification code is invalid. Obtain it again"));
+                res.status(200).json(result.error("The verification code is invalid. Obtain it again"));
                 return
             }
-            
             redis.hsetBuffer(email,"get_time",nowtime);
             redis.hsetBuffer(email,"status",2)
             const wallet = await contract.getWalletByPrivate(process.env.ERC20_USER);
