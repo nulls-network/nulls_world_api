fe278e7270785afaf33564e4c134a276ede03457
slightly-null <slightly@0x11.email>
2021-12-20 18:26:24 +0800

    update

--- api/petSell/findPage.js
+++ api/petSell/findPage.js
@@ -6,7 +6,7 @@ module.exports = async (req, res) => {
     let param = req.query;
     let offer = 0;
     let limit = 10;
-    if(param.pageSize != undefined && param != undefined){
+    if(param.pageSize != undefined && param.current != undefined){
             offer = Number((param.current-1)*param.pageSize);
             limit = Number(param.pageSize)
     }
