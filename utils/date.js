19e53d5ed0e1bee348e96ce79bc2debe8c85ce1f
slightly-null <slightly@0x11.email>
2022-01-17 11:03:18 +0800

    test

--- utils/date.js
+++ utils/date.js
@@ -4,16 +4,25 @@ let  date = {};
 
 
 date.getBlockTimeMillisecond = async function(){
-    const provider = new ethers.providers.JsonRpcProvider("https://http-testnet.huobichain.com");
+    const provider = new ethers.providers.JsonRpcProvider("https://http-testnet.hecochain.com/");
     const block = await provider.getBlock();
     return  block.timestamp*1000;
 }
 
 date.getBlockTime = async function(){
-    const provider = new ethers.providers.JsonRpcProvider("https://http-testnet.huobichain.com");
+    const provider = new ethers.providers.JsonRpcProvider("https://http-testnet.hecochain.com/");
     const block = await provider.getBlock();
     return  block.timestamp;
 }
+
+// async  function test(){
+//     let time =await  date.getBlockTime();
+//     console.log("xxxxxxxxx",time);
+// } 
+
+// test();
+
+
 
 module.exports = date;
 
